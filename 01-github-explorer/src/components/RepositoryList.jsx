c238f1add358baf4a94cb4a46796299dd30d14bd
Nathalia <nathalia.alice.silv@gmail.com>
2021-05-07 22:31:36 -0300

    Listando repositórios

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,12 +1,6 @@
 import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
-// https://api.github.com/users/nathalia-alice/repos
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: "https://github.com/unform/unform"
-}
 
 export function RepositoryList(){
     const [repositories, setRepositories] = useState([]);
@@ -21,10 +15,9 @@ export function RepositoryList(){
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
             </ul>
         </section>
     )
