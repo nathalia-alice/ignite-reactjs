8461ad28924c66dc4b99296c99b69c56d5e6d101
Nathalia <nathalia.alice.silv@gmail.com>
2021-05-27 23:12:13 -0300

    Componente: NewTransactionModal

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 
-import Modal from 'react-modal';
 
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from "./styles/global";
-
+import Modal from 'react-modal';
 Modal.setAppElement("#root");
 
 export function App() {
@@ -24,12 +24,10 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-      <Modal
+      <NewTransactionModal 
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
       <GlobalStyle />
     </>
   );
