8e022a9528e259d561cd5f06e7fbfbd2642a4b35
Nathalia <nathalia.alice.silv@gmail.com>
2021-05-31 22:08:16 -0300

    Funcionamento dos Botões

--- dtmoney/src/components/NewTransactionModal/styles.ts
+++ dtmoney/src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { darken} from 'polished';
+import { darken } from 'polished';
 
 export const Container = styled.form`
    h2{
@@ -47,39 +47,44 @@ export const Container = styled.form`
        }
 
    }
-`
+`;
 
 export const TransactionTypeContainer = styled.div`
     margin: 1rem 0;
     display: grid;
     grid-template-columns: 1fr 1fr;
     gap: 0.5rem;
+`;
 
-    button{
-        height: 4rem;
-        border: 1px solid #d7d7d7;
-        border-radius: 0.25rem;
-        background: transparent;
+interface RadioBoxProps {
+    isActive: boolean;
+}
 
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        transition: border-color 0.2s;
-        
-        &:hover{
-            border-color: ${darken(0.1, '#d7d7d7')};
-        }
+export const RadioBox = styled.button<RadioBoxProps>`
+    height: 4rem;
+    border: 1px solid #d7d7d7;
+    border-radius: 0.25rem;
+    background: ${(props) => props.isActive ? '#ccc' : 'transparent'};
 
-        img{
-            width: 20px;
-            height: 20px;
-        }
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: border-color 0.2s;
 
-        span{
-            display: inline-block;
-            margin-left: 1rem;
-            font-size: 1rem;
-            color: var(--text-title);
-        }
+    &:hover{
+        border-color: ${darken(0.1, '#d7d7d7')};
     }
-`
+
+    img{
+        width: 20px;
+        height: 20px;
+    }
+
+    span{
+        display: inline-block;
+        margin-left: 1rem;
+        font-size: 1rem;
+        color: var(--text-title);
+    }
+
+`;
